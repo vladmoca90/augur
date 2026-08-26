19c3daeef4f580332baf6b0f8e1fd2344d79fd5e
Vlad Mocanu <vladmoca90@gmail.com>
2026-08-27 00:38:01 +0100

    Working to fetch the data an ddeclar all the variables. Also declared the .filter-container class

--- app/components/LiveComponent.tsx
+++ app/components/LiveComponent.tsx
@@ -1,19 +1,45 @@
 "use client";
+import { useCallback, useEffect, useState } from "react";
 import styles from "../styles/augur.module.css";
+import { error } from "next/dist/build/output/log";
+import { Venues } from "../types/venues";
 
 const baseUrl = "https://frontend-takehome-server-production.up.railway.app";
 const venuesUrl = `${baseUrl}/api/venues`;
 const eventsUrl = `${baseUrl}/api/events/stream`;
 
 export default function LiveComponent() {
+  const [venues, setVenues] = useState<Venues[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  
+  const getVenues = useCallback(async () => {
+    try {
+      const response = await fetch(venuesUrl);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Venues data:", data);
+      setVenues(data);
+    } catch (error) {
+      console.error("Error fetching venues:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getVenues();
+  }, [getVenues]);
+
   return (
     <section className={styles["live-component"]}>
       <header>
         <h1 className={styles["header"]}>Augur exercise</h1>
       </header>
-      <div className={styles["map-container"]}>
-
-      </div>
+      <div className={styles["filter-container"]}></div>
+      <div className={styles["map-container"]}></div>
     </section>
   );
 }
